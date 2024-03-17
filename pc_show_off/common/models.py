74862dafee1fe9f2e5418e1bd646211d86ee899f
ds7cw <delyan.shivachev@hotmail.com>
2024-03-17 10:19:23 +0000

    ComputerComponent abstract parent class

--- pc_show_off/common/models.py
+++ pc_show_off/common/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+UserModel = get_user_model()
+
+class ComputerComponent(models.Model):
+
+    class Meta:
+        abstract = True
+
+    class ManufacturerChoices(models.TextChoices):
+        pass
+
+    manufacturer = models.CharField(
+        max_length=15,
+        choices = ManufacturerChoices.choices,
+    )
+
+    model_name = models.CharField(
+        max_length=50,
+        unique=True,
+    )
+
+    is_verified = models.BooleanField(
+        default=False,
+    )
+
+    contributor = models.ForeignKey(
+        UserModel,
+        on_delete=models.DO_NOTHING,
+    )
