a80709d877c0f9e4b80a79345224642a9c9f26c7
ds7cw <delyan.shivachev@hotmail.com>
2025-09-16 23:14:11 +0300

    Tweak settings.py for containerization

--- pc_show_off/settings.py
+++ pc_show_off/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == '1'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost'] # for local testing
 
 if not ALLOWED_HOSTS:
     ALLOWED_HOSTS += [os.environ.get('ALLOWED_HOSTS')]
@@ -135,11 +135,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
