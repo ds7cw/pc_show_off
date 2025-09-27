05b0f768465ae5f48c6a4008bc85ff0ce5bd418a
ds7cw <delyan.shivachev@hotmail.com>
2025-09-27 10:15:41 +0300

    Add logging to ram CRUD views

--- pc_show_off/ram/views.py
+++ pc_show_off/ram/views.py
@@ -1,3 +1,4 @@
+import logging
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model, mixins as auth_mixins
@@ -8,6 +9,8 @@ from django.urls import reverse, reverse_lazy
 from .forms import CreateRamModelForm, DeleteRamModelForm
 from .models import Ram
 
+logger = logging.getLogger(name=__name__)
+
 
 # Create your views here.
 @login_required(login_url='login-page')
@@ -16,6 +19,7 @@ def ram_create(request):
     UserModel = get_user_model()
     current_user = UserModel.objects.get(pk=request.user.pk)
     context = {'form': CreateRamModelForm()}
+    logger.info(msg="User with pk {} opened a RAM Creation Form".format(current_user.pk))
 
     if request.method == 'POST':
         if form.is_valid():
@@ -24,10 +28,16 @@ def ram_create(request):
 
             if current_user.is_staff: # If created by staff member, assume naming conventions are correct
                 new_instance.is_verified = True
+                logger.info(msg="User with pk {} verified RAM {} {} {}".format(current_user.pk,
+                    new_instance.manufacturer, new_instance.series, new_instance.model_name))
                 current_user.profile.contributions += 1
                 current_user.profile.save()
+                logger.info(msg="Contributions of user with pk {} increased to {}".format(
+                    current_user.pk, current_user.profile.contributions))
 
             new_instance.save()
+            logger.info("User with pk {} created RAM {} {} {}".format(current_user.pk,
+                new_instance.manufacturer, new_instance.series, new_instance.model_name))
             return redirect('ram-list')
 
     return render(request, 'ram/ram-create.html', context)
@@ -46,10 +56,14 @@ def ram_edit(request, ram_id):
             updated_ram = form.save(commit=False)
 
             if not updated_ram.is_verified:
-                updated_ram.is_verified = True        
+                updated_ram.is_verified = True
+                logger.info(msg="User with pk {} verified RAM {} {} {}".format(request.user.pk,
+                    updated_ram.manufacturer, updated_ram.series, updated_ram.model_name))
                 obj.contributor.profiles.contributions += 1
                 obj.contributor.profiles.save()
-        
+                logger.info(msg="Contributions of user with pk {} increased to {}".format(
+                    obj.contributor.pk, obj.contributor.profile.contributions))
+
             updated_ram.save()
             return redirect('ram-list')
 
@@ -78,14 +92,22 @@ class RamDetailView(auth_mixins.LoginRequiredMixin, views.DetailView):
 
 @staff_member_required(login_url='login-page')
 def ram_delete(request, ram_id):
-    object = Ram.objects.filter(pk=ram_id).first()
-    form = DeleteRamModelForm(instance=object)
-    context = {'form': form, 'object': object}
+    obj = Ram.objects.filter(pk=ram_id).first()
+    form = DeleteRamModelForm(instance=obj)
+    context = {'form': form, 'object': obj}
+
+    logger.warning(msg="User with pk {} opened a RAM Deletion Form for {} {} {}".format(
+        request.user.pk, obj.manufacturer, obj.series, obj.model_name))
 
     if request.method == 'POST':
-        object.delete()
+        ram_manufacturer = obj.manufacturer
+        ram_series = obj.series
+        ram_model_name = obj.model_name
+        obj.delete()
+        logger.warning(msg="RAM {} {} {} deleted".format(
+            ram_manufacturer, ram_series, ram_model_name))
         return redirect('ram-list')
-    
+
     return render(request, 'ram/ram-delete.html', context)
 
 
@@ -94,7 +116,7 @@ def ram_delete(request, ram_id):
 #     all_objects = Ram.objects.all().order_by('memory_type', 'max_frequency', 'manufacturer', 'series')
 #     verified_objects = all_objects.filter(is_verified=True)
 #     not_verified_objects = all_objects.filter(is_verified=False)
-    
+
 #     context = {'verified': verified_objects, 'not_verified': not_verified_objects}
 
 #     return render(request, 'ram/ram-list.html', context)
