d1eb89354b51f826f84b4fd4d5f00aa935110931
ds7cw <delyan.shivachev@hotmail.com>
2025-09-29 20:51:23 +0300

    Add logging to pc CRUD views

--- pc_show_off/pc/views.py
+++ pc_show_off/pc/views.py
@@ -1,3 +1,4 @@
+import logging
 from django.db.models import Avg
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
@@ -6,6 +7,8 @@ from django.contrib.admin.views.decorators import staff_member_required
 
 from .forms import CreatePcModelForm, DeletePcModelForm, PcRatingModelForm, PcCommentModelForm
 from .models import Pc, PcRating, PcComment
+
+logger = logging.getLogger(__name__)
 
 
 # Create your views here.
@@ -30,12 +33,15 @@ def pc_create(request):
     UserModel = get_user_model()
     current_user = UserModel.objects.get(pk=request.user.pk)
     context = {'form': CreatePcModelForm()}
+    logger.info(msg="User with pk {} opened a PC Creation Form".format(current_user.pk))
 
     if request.method == 'POST':
         if form.is_valid():
             new_instance = form.save(commit=False)
             new_instance.owner = current_user
             new_instance.save()
+            logger.info("User with pk {} created PC {} w/ {} & {}".format(current_user.pk,
+                new_instance.pc_name, new_instance.cpu_part, new_instance.gpu_part))
             return redirect('my-pc-list')
 
     return render(request, 'pc/pc-create.html', context)
@@ -64,7 +70,9 @@ def pc_edit(request, pc_name):
             updated_pc = form.save(commit=False)
             updated_pc.is_verified = True
             updated_pc.save()
-            
+            logger.info("User with pk {} updated PC {} w/ {} & {}".format(request.user.pk,
+                updated_pc.pc_name, updated_pc.cpu_part, updated_pc.gpu_part))
+
             return redirect('my-pc-list')
 
     return render(request, 'pc/pc-edit.html',context)
@@ -83,20 +91,30 @@ def pc_details(request, pc_name):
 @login_required(login_url='login-page')
 def pc_delete(request, pc_name):
     all_pcs = get_pc_and_select_related()
-    object = all_pcs.filter(owner__pk=request.user.pk, pc_name=pc_name).first()
-    form = DeletePcModelForm(instance=object)
-    context = {'form': form, 'object': object}
+    obj = all_pcs.filter(owner__pk=request.user.pk, pc_name=pc_name).first()
+    form = DeletePcModelForm(instance=obj)
+    context = {'form': form, 'object': obj}
+    user_id = request.user.pk
+
+    logger.warning(msg="User with pk {} opened a PC Deletion Form for {} [{}, {}, {}]".format(
+        user_id, obj.pc_name, obj.cpu_part.model_name, obj.gpu_part.short_model_name, obj.mobo_part.model_name))
 
     if request.method == 'POST':
-        object.delete()
+        pc_name = obj.pc_name
+        cpu_part = obj.cpu_part.model_name
+        gpu_part = obj.gpu_part.short_model_name
+        mobo_part = obj.mobo_part.model_name
+        obj.delete()
+        logger.warning(msg="PC {} [{}, {}, {}] deleted".format(
+            pc_name, cpu_part, gpu_part, mobo_part))
         return redirect('my-pc-list')
-    
+
     return render(request, 'pc/pc-delete.html', context)
 
 
 @login_required(login_url='login-page')
 def pc_list(request):
-    all_objects = get_pc_and_select_related()    
+    all_objects = get_pc_and_select_related()
     all_objects = all_objects.annotate(rating=Avg('ratings__rating_value'))
     context = {'objects': all_objects}
 
@@ -105,9 +123,9 @@ def pc_list(request):
 
 @login_required(login_url='login-page')
 def pc_rating(request, pc_name):
-    all_objects = get_pc_and_select_related()    
+    all_objects = get_pc_and_select_related()
     current_pc = all_objects.filter(pc_name=pc_name).first()
-    
+
     if not current_pc:
         return redirect('pc-list')
 
@@ -135,7 +153,7 @@ def pc_rating(request, pc_name):
 
 login_required(login_url='login-page')
 def pc_comment(request, pc_name):
-    all_objects = get_pc_and_select_related()    
+    all_objects = get_pc_and_select_related()
     current_pc = all_objects.filter(pc_name=pc_name).first()
     current_user = get_user_model()
     current_user = current_user.objects.get(pk=request.user.pk)
@@ -149,7 +167,7 @@ def pc_comment(request, pc_name):
             new_comment.author = current_user
             new_comment.save()
             return redirect('pc-list')
-    
+
     form = PcCommentModelForm()
     context = {'form': form, 'object': current_pc}
 
